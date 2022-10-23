0a0d1b435e3fdb94d43b0fe62bc66bd8f65334f4
GT <gt@gmail.com>
2022-10-23 14:49:15 +0100

    Correction of spelling

--- src/main/java/com/gt/test/git1/Main.java
+++ src/main/java/com/gt/test/git1/Main.java
@@ -2,6 +2,6 @@ package com.gt.test.git1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Gitler!");
+        System.out.println("Hello Hitler!");
     }
 }
