17084ffa9ac3d91d8a37f7f07cacf627f1175145
Sevka <Sevkacode@gmail.com>
2023-03-14 04:56:50 +0300

    rewrite code2

--- src/main/java/com/gt/test/git1/Main.java
+++ src/main/java/com/gt/test/git1/Main.java
@@ -1,7 +1,38 @@
-package com.gt.test.git1;
+package ru.sevka.springcourse;
 
-public class Main {
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Example {
+
     public static void main(String[] args) {
-        System.out.println("Hello Hitler!");
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//                "applicationContext.xml"
+//        );
+//
+//        context.close();
+
+
+
+        String pathI = "E:/SpringTestProject/src/main/resources/applicationContext.xml";
+        String pathO = "E:/SpringTestProject/src/main/resources/test.txt";
+
+        int b = 0;
+        try (InputStreamReader isr = new InputStreamReader(Files.newInputStream(Paths.get(pathI)), StandardCharsets.UTF_8);
+             FileWriter writer =  new FileWriter(pathO)) {
+            while (isr.ready()){
+                writer.write(isr.read());
+                writer.flush();
+                //writer.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
